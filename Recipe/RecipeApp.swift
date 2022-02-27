f988170bc4b529ecf1f01a00acdc7a25d63084a2
Nagy Gergő <nagy.gergi6@gmail.com>
2022-02-27 20:09:15 +0100

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by Gergo Nagy on 2/27/22.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
